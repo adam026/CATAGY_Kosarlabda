afd0fdbda68d5d7a623c1d73a83ad57342169e9a
adam026 <76121613+adam026@users.noreply.github.com>
2022-01-27 22:01:41 +0100

    Add project files.

---
+++ CATAGY_Kosarlabda/Csapat.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace CATAGY_Kosarlabda
+{
+    class Csapat
+    {
+        public string HazaiCsapat { get; set; }
+        public string IdegenCsapat { get; set; }
+        public int HazaiPont { get; set; }
+        public int IdegenPont { get; set; }
+        public string Helyszin { get; set; }
+        public DateTime Idopont { get; set; }
+
+        public Csapat(string sor)
+        {
+            var buff = sor.Split(';');
+            HazaiCsapat = buff[0];
+            IdegenCsapat = buff[1];
+            HazaiPont = int.Parse(buff[2]);
+            IdegenPont = int.Parse(buff[3]);
+            Helyszin = buff[4];
+            var datum = buff[5].Split('-');
+            Idopont = new DateTime(
+                year: int.Parse(datum[0]),
+                month: int.Parse(datum[1]),
+                day: int.Parse(datum[2])); 
+        }
+    }
+}
---
+++ CATAGY_Kosarlabda/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CATAGY_Kosarlabda
+{
+    internal class Program
+    {
+        static List<Csapat> csapatok = new List<Csapat>();
+        static void Main(string[] args)
+        {
+            Beolvas();
+            F03();
+            F04();
+            F05();
+            F06();
+            F07();
+        }
+
+        private static void F07()
+        {
+            Console.WriteLine("7. Feladat: ");
+            var stadionokSzerint = new Dictionary<string, int>();
+            foreach (var csapat in csapatok)
+            {
+                if (!stadionokSzerint.ContainsKey(csapat.Helyszin))
+                {
+                    stadionokSzerint.Add(csapat.Helyszin, 1);
+                }
+                else
+                {
+                    stadionokSzerint[csapat.Helyszin] += 1;
+                }
+            }
+
+            foreach (var stadion in stadionokSzerint)
+            {
+                if (stadion.Value > 20)
+                {
+                    Console.WriteLine($"\t{stadion.Key}: {stadion.Value}");
+                }
+            }
+        }
+
+        private static void F06()
+        {
+            DateTime datum = new DateTime(2004, 11, 21);
+            Console.WriteLine("6. Feladat:");
+            foreach (var csapat in csapatok)
+            {
+                if (csapat.Idopont == datum)
+                {
+                    Console.WriteLine($"\t{csapat.HazaiCsapat}-{csapat.IdegenCsapat} ({csapat.HazaiPont}:{csapat.IdegenPont})");
+                }
+            }
+        }
+
+        private static void F05()
+        {
+            var vajonMiLehet = csapatok.Where(x => x.HazaiCsapat.Contains("Barcelona")).First();
+            Console.WriteLine($"5. Feladat: A barcelonai csapat neve: {vajonMiLehet.HazaiCsapat}");
+        }
+
+        private static void F04()
+        {
+            var dontetlen = csapatok.Where(x => x.HazaiPont == x.IdegenPont).Count();
+            if (dontetlen != 0)
+            {
+                Console.WriteLine($"4. Feladat: Volt döntetlen? Igen! ");
+            }
+            else
+            {
+                Console.WriteLine($"4. Feladat: Volt döntetlen? Nem! ");
+            }
+        }
+
+        private static void F03()
+        {
+            var hazai = csapatok.Where(x => x.HazaiCsapat == "Real Madrid").Count();
+            var vendeg = csapatok.Where(x => x.IdegenCsapat == "Real Madrid").Count();
+            Console.WriteLine($"3. Feladat: Real Madrid: Hazai: {hazai}, Idegen: {vendeg}");
+        }
+
+        private static void Beolvas()
+        {
+            using (var sr = new StreamReader(@"..\..\RES\eredmenyek.csv", Encoding.UTF8))
+            {
+                var fejlec = sr.ReadLine();
+                while (!sr.EndOfStream)
+                {
+                    csapatok.Add(new Csapat(sr.ReadLine()));
+                }
+            }
+        }
+    }
+}
